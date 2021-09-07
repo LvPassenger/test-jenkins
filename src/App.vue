89060b2d6f76fa877fc45c43ecbeaad25ccbc085
lzh <zhenhui.lv@dexintec.com>
2021-09-07 13:38:45 +0800

    chore:修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="test jenkins"/>
+    <HelloWorld msg="test jenkins111"/>
   </div>
 </template>
 
